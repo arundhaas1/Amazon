6c1394c903dcc2eee94eac5958f6b2d09f968eea
Arundhaas1 <arunram.9215@gmail.com>
2021-06-06 19:17:28 +0530

    adding banner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import './App.css';
 import Header from "./Components/Header.js"
 import "tachyons"
-
+import Home from "./Components/Home.js"
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* <Home /> */}
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+    return (
+        <div className="homeComp">
+           <img className="banner" alt="banner" src="https://storage.googleapis.com/freesat-production-assets/styles/teaser_hero_image_1440x500_/cloud-storage/news/headliners/amazon-prime-video-banner.jpg?itok=zUbCf_wT" />
+        </div>
+    )
+}
+
+export default Home
