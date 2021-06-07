e101f822f95ba0cceb88fdad954dd97503f0a6c5
Arundhaas1 <arunram.9215@gmail.com>
2021-06-07 11:24:55 +0530

    adding id

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -17,18 +17,21 @@ function Home() {
                         rate="₹4,000"
                         star="⭐⭐⭐"
                         pic="https://m.media-amazon.com/images/I/61utX8kBDlL._AC_UL320_.jpg"
+                        id="1"
                     />                    
                    <Product 
                         info="IFLASH Men's & Women's UV Protected Classic Aviator Sunglasses (Black & Blue)"
                         rate="₹288"
                         star="⭐"
                         pic="https://m.media-amazon.com/images/I/515gKnUgwpL._AC_UL320_.jpg"
+                        id="2"
                    />
                     <Product 
                         info="Heyovin Diamond Crystal Touch Designer High Ball Tumbler Long Drink Glass for Drinking Water, Beer, Soda, Fresh Juice,Cocktail Long Drink Glass 300 ML (2)"
                         rate="₹499"
                         star="⭐⭐⭐⭐"
-                        pic="https://m.media-amazon.com/images/I/71BBc1qwTfL._QL65_AC_UL640_.jpg"                    
+                        pic="https://m.media-amazon.com/images/I/71BBc1qwTfL._QL65_AC_UL640_.jpg"           
+                        id="3"         
                     />
                 </div>
 
@@ -38,18 +41,21 @@ function Home() {
                     rate="₹63,090"
                     star="⭐⭐⭐"
                     pic="https://m.media-amazon.com/images/I/41ivUw9C9PS._AC_UY218_.jpg"
+                    id="4"
                    />
                     <Product 
                     info="Philips GC181 Heavy Weight 1000-Watt Dry Iron" 
                     rate="₹48,000"
                     star="⭐⭐⭐⭐"
                     pic="https://m.media-amazon.com/images/I/51qNhKjuFPL._AC_UY218_.jpg"
+                    id="5"
                     />
                     <Product 
                     info="Meditating Sitting Buddha Idols Showpiece For Home Decor - Set of 4 Matki Candles Tealight Diya Decoration Items (9 x 6.5 x 3 Inches)" 
                     rate="₹4,900"
                     star="⭐⭐⭐⭐⭐"
                     pic="https://m.media-amazon.com/images/I/71BHm4xsS-L._AC_UL320_.jpg"
+                    id="6"
                     />
                 </div>
 
@@ -59,18 +65,21 @@ function Home() {
                       rate="₹3,998"
                       star="⭐⭐⭐⭐"
                       pic="https://m.media-amazon.com/images/I/71oQGQ387tL._AC_UL320_.jpg"
+                      id="7"
                     />
                     <Product 
                       info="Champions League Club Logos Coloring Book: Top 50 Ranked Teams in Europe 2016/17: This A4 100 page Book has all the Club Logos from the Top 50 ranked ... for you to color. A must for all Soccer fans."
                       rate="₹999"
                       star="⭐⭐⭐⭐"
                       pic="https://m.media-amazon.com/images/I/814uZfYOgYL._AC_UL320_.jpg"
+                      id="8"
                     />
                     <Product 
                         info="Bourge Women's Micam-z51 Running Shoes"
                         rate="₹13,777"
                         star="⭐⭐⭐"
                         pic="https://m.media-amazon.com/images/I/71Xl7u6FaZL._AC_SX200_SY161_QL70_.jpg"
+                        id="9"
                     />
                 </div>
 
@@ -80,6 +89,7 @@ function Home() {
                     rate="₹45,000"
                     star="⭐⭐⭐⭐"
                     pic="https://m.media-amazon.com/images/I/61QLVYZGyFS._AC_UY218_.jpg"    
+                    id="10"
                     />
     
                 </div>
